867136347a5768cb3e0f15c0d2d16d29b9cfc6c7
Pedro Ferreira <plferreira4@gmail.com>
2012-11-09 02:54:09 +0000

    - add start/stop, restart and reset to the CountDown watch

--- scripts/count_down.js
+++ scripts/count_down.js
@@ -3,34 +3,59 @@ var CountDown;
     var COUNT = 0;
     var COUNT_ELEMENT;
     var INTERVAL_F;
-    function init() {
-        COUNT_ELEMENT = document.querySelector("#CountDown-count");
-        var entry = document.querySelector("#CountDown-entry");
-        var start = document.querySelector("#CountDown-start");
-        start.onclick = function () {
-            COUNT_ELEMENT.innerText = entry.value;
-            COUNT = stringToMilliseconds(entry.value);
-            startTimer();
-        };
-        var stop = document.querySelector("#CountDown-stop");
-        stop.onclick = function () {
-            stopTimer();
-        };
-    }
-    CountDown.init = init;
+    var RUNNING = false;
+    var ENTRY_ELEMENT;
     function startTimer() {
+        RUNNING = true;
         window.clearInterval(INTERVAL_F);
         INTERVAL_F = window.setInterval(tick, 1000);
     }
     function stopTimer() {
+        RUNNING = false;
         window.clearInterval(INTERVAL_F);
+    }
+    function init() {
+        COUNT_ELEMENT = document.querySelector("#CountDown-count");
+        ENTRY_ELEMENT = document.querySelector("#CountDown-entry");
+        var startStop = document.querySelector("#CountDown-startStop");
+        var restart = document.querySelector('#CountDown-restart');
+        var reset = document.querySelector('#CountDown-reset');
+        updateWatchFromEntry();
+        startStop.onclick = function () {
+            if(!RUNNING) {
+                startTimer();
+                startStop.value = 'Stop';
+            } else {
+                stopTimer();
+                startStop.value = 'Continue';
+            }
+        };
+        restart.onclick = function () {
+            startStop.value = 'Stop';
+            updateWatchFromEntry();
+            startTimer();
+        };
+        reset.onclick = function () {
+            stopTimer();
+            startStop.value = 'Start';
+            updateWatchFromEntry();
+        };
+    }
+    CountDown.init = init;
+    function updateWatchFromEntry() {
+        var value = parseInt(ENTRY_ELEMENT.value);
+        value *= 1000;
+        updateWatch(value);
     }
     function stringToMilliseconds(entryValue) {
         return entryValue * 1000;
     }
-    function tick() {
-        COUNT -= 1000;
+    function updateWatch(count) {
+        COUNT = count;
         COUNT_ELEMENT.innerText = dateToString(COUNT);
+    }
+    function tick() {
+        updateWatch(COUNT - 1000);
     }
 })(CountDown || (CountDown = {}));
 
--- scripts/count_down.ts
+++ scripts/count_down.ts
@@ -3,41 +3,21 @@
 module CountDown
 { 
 
+    // counts the number of milliseconds left in the watch
 var COUNT = 0;
 var COUNT_ELEMENT: HTMLInputElement;
 
-var INTERVAL_F;
+var INTERVAL_F: number;
 
-export function init()
-{
-COUNT_ELEMENT = <HTMLInputElement> document.querySelector( "#CountDown-count" );
+var RUNNING = false;
 
-
-var entry = <HTMLInputElement> document.querySelector( "#CountDown-entry" );
-
-var start = <HTMLInputElement> document.querySelector( "#CountDown-start" );
-
-start.onclick = function()
-    {
-    COUNT_ELEMENT.innerText = entry.value;
-
-    COUNT = stringToMilliseconds( entry.value );
-
-    startTimer();
-    };
-
-var stop = <HTMLInputElement> document.querySelector( "#CountDown-stop" );
-
-
-stop.onclick = function()
-    {
-    stopTimer();
-    };
-}
+var ENTRY_ELEMENT: HTMLInputElement;
 
 
 function startTimer()
 {
+RUNNING = true;
+
 window.clearInterval( INTERVAL_F );
 
 INTERVAL_F = window.setInterval( tick, 1000 );
@@ -46,8 +26,77 @@ INTERVAL_F = window.setInterval( tick, 1000 );
 
 function stopTimer()
 {
+RUNNING = false;
+
 window.clearInterval( INTERVAL_F );
 }
+
+
+export function init()
+{
+COUNT_ELEMENT = <HTMLInputElement> document.querySelector( "#CountDown-count" );
+
+
+ENTRY_ELEMENT = <HTMLInputElement> document.querySelector( "#CountDown-entry" );
+
+var startStop = <HTMLInputElement> document.querySelector( "#CountDown-startStop" );
+var restart = <HTMLInputElement> document.querySelector( '#CountDown-restart' );
+var reset = <HTMLInputElement> document.querySelector( '#CountDown-reset' );
+
+
+updateWatchFromEntry();
+
+startStop.onclick = function()
+    {
+        // start the watch
+    if ( !RUNNING )
+        {
+        startTimer();
+
+        startStop.value = 'Stop';
+        }
+
+        // stop the watch
+    else
+        {
+        stopTimer();
+
+        startStop.value = 'Continue';
+        }  
+    };
+
+
+restart.onclick = function()
+    {
+    startStop.value = 'Stop';
+
+    updateWatchFromEntry();
+
+    startTimer();
+    };
+
+
+reset.onclick = function()
+    {
+    stopTimer();
+
+    startStop.value = 'Start';
+
+    updateWatchFromEntry();
+    };
+}
+
+
+function updateWatchFromEntry()
+{
+var value = parseInt( ENTRY_ELEMENT.value );
+
+    // seconds to milliseconds ?
+value *= 1000;
+
+updateWatch( value );
+}
+
 
 
 function stringToMilliseconds( entryValue )
@@ -57,11 +106,17 @@ return entryValue * 1000;
 }
 
 
-function tick()
+function updateWatch( count: number )
 {
-COUNT -= 1000;
+COUNT = count;
 
 COUNT_ELEMENT.innerText = dateToString( COUNT );
+}
+
+
+function tick()
+{
+updateWatch( COUNT - 1000 );
 }
 
 }
