0c5775daba5cf8f3f14341e7cb8d1311801316b7
Pedro Ferreira <plferreira4@gmail.com>
2013-06-06 19:40:46 +0100

    - use a common create_release script for all the applications (share as much code as possible)

--- create_release/create_server_template.py
+++ create_release/create_server_template.py
@@ -1,77 +1,37 @@
-"""
-    python3
-    
-    Creates the template for the server based on the index.html
-    
-    it searches for all the scripts tags, and adds {{ STATIC_URL }}appName/
-"""
+# python3
 
 import os.path
 import argparse
+import sys
 
-from bs4 import BeautifulSoup
+sys.path.append( 'C:/Users/drk/Dropbox/projects/' )
 
 
+import create_release_script.create_server_template as create_server_template
+
+
+    # relative paths
 default_indexPath = '../index.html'
 default_appName = 'timer'
-default_copyToPath = '../../../website/templates/{}/'.format( default_appName )
+default_copyToPath = 'C:/Users/drk/Dropbox/projects/website/templates/{}/'.format( default_appName )
 
 
-def createTemplate( indexPath= default_indexPath, appName= default_appName, copyToPath= default_copyToPath, templateName= None ):
-
-    indexPath = os.path.join( os.path.dirname(__file__), indexPath )
-    copyToPath = os.path.join( os.path.dirname(__file__), copyToPath )
-
-
-    with open( indexPath, 'r' ) as f:
-        content = f.read()
+def go( indexPath= default_indexPath,
+        appName= default_appName,
+        copyToPath= default_copyToPath,
+        templateName= None ):
 
 
-    soup = BeautifulSoup( content )
+    indexPath = os.path.realpath( indexPath )
+    copyToPath = os.path.realpath( copyToPath )
 
-        # deal with all the <script>
-
-    allScripts = soup.find_all( 'script' )
-
-    for script in allScripts:
-
-        script['src'] = '{{ STATIC_URL }}' + appName + '/' + script['src']
-
-
-        # deal with all the <link>
-
-    allLinks = soup.find_all( 'link' )
-
-    for link in allLinks:
-
-        link[ 'href' ] = '{{ STATIC_URL }}' + appName + '/' + link[ 'href' ]
-
-
-        # add a <script> at the end of the <head>
-    newScript = soup.new_tag( 'script' )
-
-    newScript.string = "var TYPE = 'server';\nvar BASE_URL = '{{ STATIC_URL }}%s/';" % appName
-
-    soup.find( 'head' ).append( newScript )
-
-
-        # create the new file
-    if not templateName:
-        fileName = os.path.basename( indexPath )
-        
-    else:
-        fileName = templateName
-        
-
-    with open( os.path.join( copyToPath, fileName ), 'w' ) as f:
-
-        f.write( soup.prettify() )
+    create_server_template.go( indexPath, appName, copyToPath, templateName )
 
 
 
 if __name__ == '__main__':
 
-    parser = argparse.ArgumentParser( description='Create the server template.' )
+    parser = argparse.ArgumentParser( description= 'Create the server template.' )
 
     parser.add_argument( 'indexPath', nargs= '?', default= default_indexPath )
     parser.add_argument( 'appName', nargs= '?', default= default_appName )
@@ -79,4 +39,4 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    createTemplate( args.indexPath, args.appName, args.copyToPath )
+    go( args.indexPath, args.appName, args.copyToPath )
--- create_release/main.py
+++ create_release/main.py
@@ -1,196 +1,50 @@
-"""
-    Python3
-"""
-
+# python3
 
 import argparse
+import sys
 import os.path
-import shutil
-import re
-import os
-import distutils
-import distutils.archive_util
 
-import copy_files
-import generate_config
-import concatenate_files
-import optimize
-import create_server_template
+sys.path.append( 'C:/Users/drk/Dropbox/projects/' )
+
+import create_release_script.main as main
 
 
+    # relative paths
 default_htmlFile = "../index.html"
-default_concatenateConfig = "config.txt"
 default_copyFilesConfig = "copy_files_config.txt"
+default_concatenateConfig = "config.txt"
+default_resultingFolder = 'timer'
 
 
 def go( htmlFile= default_htmlFile,
         copyFilesConfig= default_copyFilesConfig,
-        concatenateConfig= default_concatenateConfig ):
-    """
-        Arguments:
-          copyFilesConfig (str) : path to the config file with the files to copy
-    """
+        concatenateConfig= default_concatenateConfig,
+        resultingFolder= default_resultingFolder ):
 
-            # to guarantee that the paths are relative to the current file's path (and not the working directory)
-    htmlFile = os.path.join( os.path.dirname(__file__), htmlFile )
-    concatenateConfig = os.path.join( os.path.dirname(__file__), concatenateConfig )
-    copyFilesConfig = os.path.join( os.path.dirname(__file__), copyFilesConfig )
-    resultingFolder = os.path.join( os.path.dirname(__file__), 'timer' )
+        # absolute paths
+    htmlFile = os.path.realpath( htmlFile )
+    copyFilesConfig = os.path.realpath( copyFilesConfig )
+    concatenateConfig = os.path.realpath( concatenateConfig )
+    resultingFolder = os.path.realpath( resultingFolder )
 
-
-    cleanPreviousRun( resultingFolder )
-
-    os.makedirs( resultingFolder )
-
-    copy_files.copyFiles( copyFilesConfig )
-
-    generate_config.generate( htmlFile, concatenateConfig )
-
-    concatenatedFileName = 'minimized.js'
-    concatenatedFilePath = os.path.join( resultingFolder, concatenatedFileName )
+    baseDirectory = os.path.realpath( '' )
 
 
-    concatenate_files.concatenate( concatenateConfig, concatenatedFilePath )
+    main.go( htmlFile, copyFilesConfig, concatenateConfig, resultingFolder, baseDirectory )
 
-
-    removeStrict( concatenatedFilePath )
-
-        # run it through closure compiler to optimize (the javascript file)
-    optimize.js( concatenatedFilePath )
-
-
-    createNewIndex( htmlFile, concatenatedFileName, os.path.join( resultingFolder, "index.html" ) )
-
-
-        # zip the folder
-    compressFolder( resultingFolder )
 
     
-
-    
-def cleanPreviousRun( resultingFolder ):
-    """
-        Remove the files created from a previous run of this script (the whole folder, basically)
-
-        Arguments:
-
-            path (str) : path to the folder
-    """
-
-        # remove the folder
-    if os.path.isdir( resultingFolder ):
-        shutil.rmtree( resultingFolder )
-
-    zip = resultingFolder + '.zip'
-        
-        # and the .zip file
-    if os.path.isdir( zip ):
-        shutil.rmtree( zip )
-
-
-
-
-            
-'''
-    Creates a compressed folder with the todolist release
-'''
-    
-def compressFolder( resultingFolder ):
-
-    previousPath = os.getcwd()
-
-    distutils.archive_util.make_archive(resultingFolder, 'zip', os.path.join(previousPath, resultingFolder))
-    
-
-
-
-
-def removeStrict( jsFile ):
-
-    '''
-    Removes the 'use strict'; (but the first one) -- closure complains about it
-    '''
-    
-    with open( jsFile, 'r' ) as f:
-        
-        content = f.read()
-    
-        
-        # replace 'use strict'; or "use strict"; for empty string
-    newContent = re.sub( '[\'\"]use strict[\'\"];', '', content )
-    
-    
-        # add just one in the beginning
-    newContent = "'use strict';" + newContent
-    
-    
-    with open( jsFile, 'w' ) as f:
-        
-        f.write( newContent )
-    
-        
-        
-        
-'''
-    Removes the part between the flags, and adds a single script with the concatenated file
-    
-    Arguments:
-        
-        indexPath        (string) : name of the index.html file
-        concatenatedFile (string) : path to the concatenated file just created
-        newIndexName     (string) : name of the new file that is created
-'''
-
-def createNewIndex( indexPath, concatenatedFile, newIndexName ):
-    
-        # get the file's content
-    index = open( indexPath, 'r' )
-    
-    text = index.read()
-    
-    index.close()
-    
-    startFlag = "<!-- CONCATENATE_START -->"
-    endFlag   = "<!-- CONCATENATE_END -->"
-    
-        # find the position where the flags are
-
-    start = text.find( startFlag )
-    end = text.find( endFlag, start )
-
-
-        # get the part before the start flag
-    textBefore = text[ : start ]
-    
-        # and after the end flag
-    textAfter = text[ end + len(endFlag) : ]
-    
-    
-        # create the new file
-    newIndexName = os.path.normpath( newIndexName )
-
-    newIndex = open( newIndexName, 'w' )
-    
-    newIndex.write( textBefore + '<script type="text/javascript" src="' + concatenatedFile + '"></script>' + textAfter )
-    
-    newIndex.close()
-    
-    print( "Created file:", newIndexName )
-    
-        
 
 if __name__ == '__main__':
 
-    parser = argparse.ArgumentParser( description='Create the release of the website plus the applications associated' )
+    parser = argparse.ArgumentParser( description= 'Generate the release files of the program.' )
 
-    
-    parser.add_argument( 'htmlFile', help='Path to the home.html.', nargs='?', default= default_htmlFile )
+    parser.add_argument( 'htmlFile', help= 'Path to the index.html.', nargs= '?', default= default_htmlFile )
+    parser.add_argument( 'copyFilesConfig', help= 'Path to the configuration file that tells which files to copy.', nargs= '?', default= default_copyFilesConfig )
+    parser.add_argument( 'concatenateConfig', help= 'Path to the configuration file to tell which files to concatenate (and the order).', nargs= '?', default= default_concatenateConfig )
+    parser.add_argument( 'resultingFolder', help= "Name of the folder that is created in the current path and contains the copies.", nargs= "?", default= default_resultingFolder )
 
-    parser.add_argument( 'copyFilesConfig', help='Path to the config file with the files to copy', nargs='?', default= default_copyFilesConfig )
-
-    parser.add_argument( 'concatenateConfig', help='Path to the configuration file to tell which files to concatenate (and the order).', nargs='?', default= default_concatenateConfig )
 
     args = parser.parse_args()
 
-    go( args.htmlFile, args.copyFilesConfig, args.concatenateConfig )
-
+    go( args.htmlFile, args.copyFilesConfig, args.concatenateConfig, args.resultingFolder )
