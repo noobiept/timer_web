2824280bbfb19cf62f782cd0fefd662bf937a740
Pedro Ferreira <plferreira4@gmail.com>
2016-02-26 22:21:23 +0000

    - remove the 'save_load.ts' file     - load the watches in the 'initApp()' function

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,19 @@
 /// <reference path="../d.ts/jquery.d.ts" />
 /// <reference path="../d.ts/jqueryui.d.ts" />
 /// <reference path="stopwatch.ts" />
-/// <reference path="save_load.ts" />
 /// <reference path="menu.ts" />
 window.onload = function () {
     Data.load(initApp);
 };
 function initApp() {
     StopWatch.init();
-    var loadSuccessful = loadWatches();
-    // add some watches if its the first time the program is running
-    if (!loadSuccessful) {
+    var watches = Data.getWatches();
+    if (watches.length > 0) {
+        for (var i = 0; i < watches.length; i++) {
+            new StopWatch(watches[i]);
+        }
+    }
+    else {
         new StopWatch({ countUp: true });
         new StopWatch({ countUp: false });
     }
--- scripts/main.ts
+++ scripts/main.ts
@@ -2,7 +2,6 @@
 /// <reference path="../d.ts/jqueryui.d.ts" />
 
 /// <reference path="stopwatch.ts" />
-/// <reference path="save_load.ts" />
 /// <reference path="menu.ts" />
 
 
@@ -16,10 +15,18 @@ function initApp()
 {
 StopWatch.init();
 
-var loadSuccessful = loadWatches();
+var watches = Data.getWatches();
+
+if ( watches.length > 0 )
+    {
+    for (var i = 0 ; i < watches.length ; i++)
+        {
+        new StopWatch( watches[ i ] );
+        }
+    }
 
     // add some watches if its the first time the program is running
-if ( !loadSuccessful )
+else
     {
     new StopWatch( { countUp: true } );
     new StopWatch( { countUp: false } );
