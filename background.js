3ae9233e1c2b2a14fc0721240bcbafa0dd781202
Pedro Ferreira <plferreira4@gmail.com>
2016-04-05 18:30:29 +0100

    - add an 'id' to the chrome app window, this will make chrome remember the size/position on the window

--- background.js
+++ background.js
@@ -1,5 +1,6 @@
 chrome.app.runtime.onLaunched.addListener( function() {
     chrome.app.window.create( 'index.html', {
-        'state': 'maximized'
+        'state': 'maximized',
+        'id': 'default'
     });
 });
