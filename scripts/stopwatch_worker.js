cdae1f504e1088664c8af8d7c1f6c6d07ae55784
Pedro Ferreira <plferreira4@gmail.com>
2016-12-07 02:25:26 +0000

    - remove the compiled javascript files from the repository (only keep the typescript files)

--- scripts/stopwatch_worker.js
+++
@@ -1,5 +0,0 @@
-var timerInterval = 100; // 0.1 seconds
-// we're setting an interval in a web worker instead of in the main thread, because when the webpage is inactive the browser throttles the javascript execution (that doesn't happen in the web worker thread)
-setInterval(function () {
-    postMessage(1);
-}, timerInterval);
